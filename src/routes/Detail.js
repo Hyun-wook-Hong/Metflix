8eb55c46a8faf039d3049a94faef7c17b6cf9740
Jeremy Hong <61657215+Hyun-wook-Hong@users.noreply.github.com>
2021-05-01 18:53:29 +0900

    5/1 Detail 카드 컨텐츠 추가 patch

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Proptypes from "prop-types";
+import "./Details.css";
 
 class Detail extends React.Component{
     componentDidMount(){
@@ -12,7 +14,15 @@ class Detail extends React.Component{
     render(){
         const { location } = this.props;
         if(location.state){
-            return <span> { location.state.title }</span> ;
+            return (
+                <div className="detail__container">
+                    <img src={ location.state.poster } alt={ location.state.title } title={ location.state.title }/>
+                    <h3> { location.state.title }</h3> 
+                    <h5> { location.state.year } </h5>
+                    <ul> {location.state.genres.map((genre, index) => (<li key={index} className="genres__genre">{genre}</li>)) } </ul>
+                    <p>{ (location.state.summary)}</p>
+                </div>
+                );
         }else{
             return null;
         }
